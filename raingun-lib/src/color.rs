9f4c0dbe6ab93d7c498aa479ebe6a28c77a2b7dd
Magnus Bergmark <magnus.bergmark@gmail.com>
2017-04-05 21:00:54 +0200

    Color can be read from strings like "#aabbcc"

--- raingun-lib/src/color.rs
+++ raingun-lib/src/color.rs
@@ -2,7 +2,7 @@ use std::ops::{Add, Mul};
 
 use image::{Pixel, Rgba};
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Color {
     pub red: f32,
     pub green: f32,
@@ -10,20 +10,22 @@ pub struct Color {
 }
 
 impl Color {
-    pub fn black() -> Color {
+    pub fn new(red: f32, green: f32, blue: f32) -> Color {
         Color {
-            red: 0.0,
-            green: 0.0,
-            blue: 0.0,
+            red: red,
+            green: green,
+            blue: blue,
         }
     }
 
+    pub fn black() -> Color {
+        Color::new(0.0, 0.0, 0.0)
+    }
+
     pub fn from_rgba(rgba: Rgba<u8>) -> Color {
-        Color {
-            red: rgba.data[0] as f32 / 255.0,
-            green: rgba.data[1] as f32 / 255.0,
-            blue: rgba.data[2] as f32 / 255.0,
-        }
+        Color::new(rgba.data[0] as f32 / 255.0,
+                   rgba.data[1] as f32 / 255.0,
+                   rgba.data[2] as f32 / 255.0)
     }
 
     pub fn rgba(&self) -> Rgba<u8> {
@@ -34,11 +36,25 @@ impl Color {
     }
 
     pub fn clamp(&self) -> Color {
-        Color {
-            red: self.red.min(1.0).max(0.0),
-            green: self.green.min(1.0).max(0.0),
-            blue: self.blue.min(1.0).max(0.0),
-        }
+        Color::new(self.red.min(1.0).max(0.0),
+                   self.green.min(1.0).max(0.0),
+                   self.blue.min(1.0).max(0.0))
+    }
+}
+
+impl Default for Color {
+    fn default() -> Color {
+        Color::black()
+    }
+}
+
+impl ::std::fmt::Display for Color {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        let red_byte = (self.red as f32 * 255.0).floor() as u8;
+        let green_byte = (self.green as f32 * 255.0).floor() as u8;
+        let blue_byte = (self.blue as f32 * 255.0).floor() as u8;
+
+        write!(f, "#{:02x}{:02x}{:02x}", red_byte, green_byte, blue_byte)
     }
 }
 
@@ -46,11 +62,9 @@ impl Add for Color {
     type Output = Color;
 
     fn add(self, other: Color) -> Color {
-        Color {
-            red: self.red + other.red,
-            green: self.green + other.green,
-            blue: self.blue + other.blue,
-        }
+        Color::new(self.red + other.red,
+                   self.green + other.green,
+                   self.blue + other.blue)
     }
 }
 
@@ -58,11 +72,9 @@ impl Mul for Color {
     type Output = Color;
 
     fn mul(self, other: Color) -> Color {
-        Color {
-            red: self.red * other.red,
-            green: self.green * other.green,
-            blue: self.blue * other.blue,
-        }
+        Color::new(self.red * other.red,
+                   self.green * other.green,
+                   self.blue * other.blue)
     }
 }
 
@@ -86,11 +98,7 @@ impl Mul<f32> for Color {
     type Output = Color;
 
     fn mul(self, other: f32) -> Color {
-        Color {
-            red: self.red * other,
-            green: self.green * other,
-            blue: self.blue * other,
-        }
+        Color::new(self.red * other, self.green * other, self.blue * other)
     }
 }
 
@@ -99,5 +107,48 @@ impl<'a> Mul<f32> for &'a Color {
 
     fn mul(self, other: f32) -> Color {
         (*self) * other
+    }
+}
+
+#[test]
+fn it_parses_strings() {
+    use std::str::FromStr;
+    assert_eq!(Color::from_str("#000000").unwrap(),
+               Color {
+                   red: 0.0,
+                   green: 0.0,
+                   blue: 0.0,
+               });
+
+    assert_eq!(Color::from_str("#ffffff").unwrap(),
+               Color {
+                   red: 1.0,
+                   green: 1.0,
+                   blue: 1.0,
+               });
+
+    assert_eq!(Color::from_str("#ff7f11").unwrap(),
+               Color {
+                   red: 1.0,
+                   green: 0.498039216,
+                   blue: 0.066666667,
+               });
+}
+
+#[test]
+fn it_displays_strings() {
+    assert_eq!(Color::black().to_string(), "#000000");
+    assert_eq!(Color::new(1.0, 0.5, 0.0).to_string(), "#ff7f00");
+}
+
+#[test]
+fn it_returns_same_color_as_input() {
+    use std::str::FromStr;
+    let samples = &["#000000", "#123456", "#ffeecc", "#fef0fa", "#010203"];
+
+    for sample in samples {
+        let parsed_color: Color = sample.parse().unwrap();
+        let back_again = sample.to_string();
+        assert_eq!(&back_again, sample);
     }
 }

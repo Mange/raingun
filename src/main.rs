dd5d226f9bf623c18ce7174729c152bccdb69ad9
Magnus Bergmark <magnus.bergmark@gmail.com>
2017-04-17 21:15:55 +0200

    Add quick options for common resolutions

--- src/main.rs
+++ src/main.rs
@@ -12,28 +12,31 @@ use clap::Arg;
 extern crate raingun_lib as raingun;
 use raingun::Scene;
 
-const DEFAULT_WIDTH: u32 = 800;
-const DEFAULT_HEIGHT: u32 = 600;
-const DEFAULT_WIDTH_STR: &'static str = "800";
-const DEFAULT_HEIGHT_STR: &'static str = "600";
-
 extern crate serde_yaml;
 use std::fs::File;
 
-fn main() {
-    let matches = app_from_crate!()
+fn construct_app<'a, 'b>() -> clap::App<'a, 'b> {
+    app_from_crate!()
         .arg(Arg::with_name("width")
                  .short("w")
                  .long("width")
                  .value_name("PIXELS")
-                 .help("Width of output image.")
-                 .default_value(DEFAULT_WIDTH_STR))
+                 .help("Width of output image."))
         .arg(Arg::with_name("height")
                  .short("h")
                  .long("height")
                  .value_name("PIXELS")
-                 .help("Height of output image.")
-                 .default_value(DEFAULT_HEIGHT_STR))
+                 .help("Height of output image."))
+        .arg(Arg::with_name("4k")
+             .long("4k")
+             .help("Renders in 4K resolution. Explicit width/height overrides.")
+             .overrides_with("hd")
+        )
+        .arg(Arg::with_name("hd")
+             .long("hd")
+             .help("Renders in 1080 (HD) resolution. Explicit width/height overrides.")
+             .overrides_with("4k")
+        )
         .arg(Arg::with_name("output")
                  .short("o")
                  .long("output")
@@ -47,7 +50,52 @@ fn main() {
                  .value_name("FILE")
                  .required(true)
                  .help("The scene definition file, in YAML format."))
-        .get_matches();
+}
+
+
+struct RenderOptions {
+    width: u32,
+    height: u32,
+}
+
+impl Default for RenderOptions {
+    fn default() -> RenderOptions {
+        RenderOptions {
+            width: 800,
+            height: 600,
+        }
+    }
+}
+
+impl<'a, 'b> From<&'b clap::ArgMatches<'a>> for RenderOptions {
+    fn from(matches: &clap::ArgMatches<'a>) -> RenderOptions {
+        let mut options = RenderOptions::default();
+
+        if matches.is_present("hd") {
+            options.width = 1920;
+            options.height = 1080;
+        } else if matches.is_present("4k") {
+            options.width = 3840;
+            options.height = 2160;
+        }
+
+        if let Some(width) = matches.value_of("width") {
+            let width = width.parse().expect("Could not parse width");
+            options.width = width;
+        }
+
+        if let Some(height) = matches.value_of("height") {
+            let height = height.parse().expect("Could not parse height");
+            options.height = height;
+        }
+
+        options
+    }
+}
+
+fn main() {
+    let matches = construct_app().get_matches();
+    let render_options = RenderOptions::from(&matches);
 
     let input_path = Path::new(matches.value_of("input").unwrap());
     let output_path = match matches.value_of("output") {
@@ -66,19 +114,8 @@ fn main() {
     let file = File::open(input_path).expect("Could not open input file");
     let scene: Scene = serde_yaml::from_reader(&file).expect("Could not load YAML");
 
-    let width = matches
-        .value_of("width")
-        .unwrap()
-        .parse()
-        .unwrap_or(DEFAULT_WIDTH);
-    let height = matches
-        .value_of("height")
-        .unwrap()
-        .parse()
-        .unwrap_or(DEFAULT_HEIGHT);
-
     let render_start = PreciseTime::now();
-    let image = scene.render(width, height);
+    let image = scene.render(render_options.width, render_options.height);
     let render_end = PreciseTime::now();
 
     image.save(&output_path).expect("Could not encode image");
@@ -105,5 +142,43 @@ fn format_duration(duration: Duration) -> String {
 
             format!("{}m {:.2}s", minutes, ms_left as f32 / 1000.0)
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn parse_arguments<'a>(args: &[&'static str]) -> clap::ArgMatches<'a> {
+        let app = construct_app();
+        app.get_matches_from(args)
+    }
+
+    #[test]
+    fn it_parses_resolution_arguments() {
+        let matches = parse_arguments(&["x", "file"]);
+        let render_options = RenderOptions::from(&matches);
+        assert_eq!(render_options.width, 800);
+        assert_eq!(render_options.height, 600);
+
+        let matches = parse_arguments(&["x", "--width", "640", "--height", "480", "file"]);
+        let render_options = RenderOptions::from(&matches);
+        assert_eq!(render_options.width, 640);
+        assert_eq!(render_options.height, 480);
+
+        let matches = parse_arguments(&["x", "--hd", "file"]);
+        let render_options = RenderOptions::from(&matches);
+        assert_eq!(render_options.width, 1920);
+        assert_eq!(render_options.height, 1080);
+
+        let matches = parse_arguments(&["x", "--hd", "--4k", "file"]);
+        let render_options = RenderOptions::from(&matches);
+        assert_eq!(render_options.width, 3840);
+        assert_eq!(render_options.height, 2160);
+
+        let matches = parse_arguments(&["x", "--hd", "--width", "2000", "file"]);
+        let render_options = RenderOptions::from(&matches);
+        assert_eq!(render_options.width, 2000);
+        assert_eq!(render_options.height, 1080);
     }
 }
